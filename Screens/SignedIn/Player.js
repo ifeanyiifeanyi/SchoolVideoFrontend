79255172f5304282bda3e610f133c04e82295192
Ifeanyi Nnaemego <josephifeanyid2@gmail.com>
2023-05-11 10:44:31 +0100

    up

--- Screens/SignedIn/Player.js
+++ Screens/SignedIn/Player.js
@@ -1,15 +1,23 @@
-import React, {useState, useRef, useEffect} from 'react';
-import {View, StyleSheet, Button, Text, Dimensions} from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, StyleSheet, Button, Text, Dimensions, TouchableOpacity, Image, ScrollView, ActivityIndicator } from 'react-native';
 import { Video, ResizeMode } from 'expo-av';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import { BASE_URL } from '@env';
 
 
 
-const Player = ({navigation, route}) => {
+const Player = ({ navigation, route }) => {
     const [feeds, setFeeds] = useState(route.params.item);
-    
-     // set video orientation to landscape or portrait
-     function setOrientation() {
+    const dateObj = new Date(feeds.created_at);
+    const formattedDate = dateObj.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    // set video orientation to landscape or portrait
+    function setOrientation() {
         if (Dimensions.get('window').height > Dimensions.get('window').width) {
             //Device is in portrait mode, rotate to landscape mode.
             ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
@@ -24,27 +32,43 @@ const Player = ({navigation, route}) => {
     const [status, setStatus] = useState({});
     return (
         <View style={styles.container}>
-        <Video
-          ref={video}
-          style={styles.video}
-          source={{
-            uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-          }}
-          useNativeControls
-          resizeMode={ResizeMode.COVER}
-          isLooping
-          onPlaybackStatusUpdate={status => setStatus(() => status)}
-          onFullscreenUpdate={setOrientation}
-        />
-        <View style={styles.buttons}>
-          <Button
-            title={status.isPlaying ? 'Pause' : 'Play'}
-            onPress={() =>
-              status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-            }
-          />
+        {
+            feeds.video && feeds.video === null ?
+            <ActivityIndicator size={'large'} color={'#2fb8f0'} /> 
+            :
+            <Video
+                ref={video}
+                style={styles.video}
+                source={{
+                    uri: `${BASE_URL}/${feeds.video}`,
+                }}
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+                onPlaybackStatusUpdate={status => setStatus(() => status)}
+                onFullscreenUpdate={setOrientation}
+            />
+        }
+
+            <View style={styles.buttons}>
+                <TouchableOpacity
+                    onPress={() =>
+                        status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+                    }
+                ><Text>{status.isPlaying ? 'Pause' : 'Play'}</Text></TouchableOpacity>
+            </View>
+            <ScrollView>
+                <View style={styles.container2}>
+                    <Text style={styles.title}>{feeds.title}</Text>
+                    <View style={styles.meta}>
+                        <Text style={styles.author}>{feeds.category.category}</Text>
+                        <Text style={styles.date}>{formattedDate}</Text>
+                    </View>
+                    <Image source={{ uri: `${BASE_URL}/${feeds.thumbnail}` }} style={styles.image} />
+                    <Text  style={styles.content}>{feeds.description}</Text>
+                </View>
+            </ScrollView>
         </View>
-      </View>
     );
 }
 
@@ -52,15 +76,63 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        backgroundColor: 'red'
+        justifyContent: 'center'
     },
-    video:{
+    video: {
         width: '100%',
         height: 300,
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf: 'center'
-    }
+    },
+    buttons: {
+        width: '60%',
+        height: 50,
+        backgroundColor: '#73a9c2',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15,
+        marginBottom: 30,
+        borderRadius: 50
+    },
+    container2: {
+        justifyContent: 'center',
+        flex: 1,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginLeft: 10
+    },
+    meta: {
+        flexDirection: 'row',
+        marginBottom: 20,
+    },
+    author: {
+        fontSize: 14,
+        color: '#999',
+        marginRight: 10,
+        marginLeft: 10,
+        color: '#acac16'
+    },
+    date: {
+        fontSize: 14,
+        color: '#acac16',
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        marginBottom: 20,
+        objectFit: 'contain'
+    },
+    content: {
+        fontSize: 16,
+        marginTop: 20,
+        padding: 8,
+        textAlign: "justify",
+        letterSpacing: -0.5
+    },
 })
 
 export default Player;
